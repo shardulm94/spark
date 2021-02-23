af6719c4a374785fb32d9512ba96276514cf5e25
Shardul Mahadik <smahadik@linkedin.com>
2021-02-23 06:25:50 -0800

    Fix tests [Take 2]

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
@@ -1224,10 +1224,10 @@ class HiveQuerySuite extends HiveComparisonTest with SQLTestUtils with BeforeAnd
   test("SPARK-33084: Add jar support Ivy URI in SQL") {
     val testData = TestHive.getHiveFile("data/files/sample.json").toURI
     withTable("t") {
-      // exclude org.pentaho:pentaho-aggdesigner-algorithm as this transitive dependency does
-      // not exist on mavencentral and hence cannot be found in the test environment
+      // do not include transitive dependencies as some do not exist on mavencentral
+      // and hence cannot be found in the test environment
       sql(s"ADD JAR ivy://org.apache.hive.hcatalog:hive-hcatalog-core:$hiveVersion" +
-        "?exclude=org.pentaho:pentaho-aggdesigner-algorithm")
+        "?transitive=false")
       sql(
         """CREATE TABLE t(a string, b string)
           |ROW FORMAT SERDE 'org.apache.hive.hcatalog.data.JsonSerDe'""".stripMargin)
